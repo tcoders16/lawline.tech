22bf915c81fdc1c34a31f0b791764f32adc5bbd0
tcoders16 <tcoders08164002@gmail.com>
2025-08-21 21:12:01 -0400

    navbar updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ export default {
     screens: {
       sm: "640px",
       md: "768px",
-      lg: "1100px", // changed from default 1024px to 1100px
+      lg: "1100px",
       xl: "1280px",
+      hd: "1520px",   // or "1440px"
       "2xl": "1536px",
     },
   },
